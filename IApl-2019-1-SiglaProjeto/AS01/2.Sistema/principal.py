6d64a680278f782331a14a680b4528fcb1c46077
Márcio Flores <marciogabriel07@gmail.com>
2019-04-08 19:58:09 -0300

    Escrita delimitada

--- IApl-2019-1-SiglaProjeto/AS01/2.Sistema/principal.py
+++ IApl-2019-1-SiglaProjeto/AS01/2.Sistema/principal.py
@@ -7,6 +7,11 @@ from prestador_especialidade import Prestador_Especialidade
 
 #Inicializando conexão com o banco de dados POSTGRESQL:
 
+tamanho_codigo_prestador = 0
+tamanho_nome_prestador = 0
+tamanho_codigo_especialidade = 0
+tamanho_descricao_especialidade = 0
+
 conn = psycopg2.connect("dbname=teste user=postgres password=postgres")
 cur = conn.cursor()
 
@@ -32,6 +37,7 @@ def obtenha_nome_prestador(cur, index):
     nome_prestador = cur.execute("SELECT pre_nome from dbo.Prestador WHERE pre_codigo = {}".format(
         obtenha_prestador_especialidade(cur, 0)[index]))
     nome_prestador = cur.fetchall()[0][0]
+    tamanho_nome_prestador = len(nome_prestador)
 
     return nome_prestador
 
@@ -39,6 +45,7 @@ def obtenha_codigo_prestador(cur, index):
     codigo_prestador = cur.execute("SELECT pre_codigo from dbo.Prestador WHERE pre_nome = '{}'".format(
         obtenha_prestadores(cur, 1)[index]))
     codigo_prestador = cur.fetchall()[0][0]
+    tamanho_codigo_prestador = len(codigo_prestador)
 
     return codigo_prestador
 
@@ -46,6 +53,7 @@ def obtenha_descricao_especialidade(cur,index):
     descricao_especialidade = cur.execute("SELECT esp_descricao from dbo.Especialidade WHERE esp_codigo = '{}'".format(
         obtenha_prestador_especialidade(cur, 1)[index]))
     descricao_especialidade = cur.fetchall()[0][0]
+    tamanho_descricao_especialidade = len(descricao_especialidade)
 
     return descricao_especialidade
 
@@ -53,8 +61,13 @@ def obtenha_codigo_especialidade(cur, index):
     codigo_especialidade = cur.execute("SELECT esp_codigo from dbo.Especialidade WHERE esp_descricao = '{}'".format(
         obtenha_especialidades(cur, 1)[index]))
     codigo_especialidade = cur.fetchall()[0][0]
+    tamanho_codigo_especialidade = len(codigo_especialidade)
 
     return codigo_especialidade
+
+#Gravando no banda de dados:
+
+def grave_codigo_prestador():
 
 #Escrevendo no arquivo no formato UTF-8:
 
@@ -67,13 +80,17 @@ def gravar_arquivo(cur):
     for index in range(number_rows):
         
         file.write(str(obtenha_codigo_prestador(cur, index)))
-        file.write("\t")
+        for index2 in range(10 - tamanho_codigo_prestador):
+            file.write(" ")
         file.write(obtenha_nome_prestador(cur, index))
-        file.write("\t")
+        for index3 in range(100 - tamanho_nome_prestador):
+            file.write(" ")
         file.write(str(obtenha_codigo_especialidade(cur, index)))
-        file.write("\t")
+        for index4 in range(8 - tamanho_codigo_especialidade):
+            file.write(" ")
         file.write(obtenha_descricao_especialidade(cur, index))
-        file.write("\n")
+        for index5 in range(30 - tamanho_descricao_especialidade):
+            file.write(" ")
     
     file.close()
 

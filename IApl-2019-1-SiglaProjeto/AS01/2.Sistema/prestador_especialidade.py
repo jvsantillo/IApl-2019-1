314c11c5f90fd3c0581a51e9f99e479adc804005
caioromao <cromao21@gmail.com>
2019-04-29 14:59:24 -0300

    Atualizando atributos "PrestadorEspecialidade"
    Atualizando atributos "PrestadorEspecialidade"

--- IApl-2019-1-SiglaProjeto/AS01/2.Sistema/prestador_especialidade.py
+++ IApl-2019-1-SiglaProjeto/AS01/2.Sistema/prestador_especialidade.py
@@ -1,35 +1,35 @@
 from prestador import Prestador
 from especialidade import Especialidade
 
-class Prestador_Especialidade:
+class PrestadorEspecialidade:
 
  #Construtor:
-    def __init__ (self, prestador, especialidade, dt_ini, dt_exc):
-        self.pre_codigo = prestador.get_pre_codigo()
-        self.esp_codigo = especialidade.get_esp_codigo()
-        self.dt_ini = dt_ini
-        self.dt_exc = dt_exc
+    def __init__ (self, ID, prestadorEspecialidadeId, DataInclusao, DataExclusao):
+        self.PrestadorID = prestador.get_PrestadorID()
+        self.EspecialidadeID = especialidade.get_EspecialidadeID()
+        self.DataInclusao = DataInclusao
+        self.DataExclusao = DataExclusao
 
  #Métodos Get e Set:
 
-    def get_pre_codigo(self):
-        return self.pre_codigo
-    def set_pre_codigo(self,pre_codigo):
-        self.pre_codigo = pre_codigo
+    def get_PrestadorID(self):
+        return self.PrestadorID
+    def set_PrestadorID(self,PrestadorID):
+        self.PrestadorID = PrestadorID
 
-    def get_esp_codigo(self):
-        return self.esp_codigo
-    def set_esp_codigo(self,esp_codigo):
-        self.esp_codigo = esp_codigo
+    def get_EspecialidadeID(self):
+        return self.EspecialidadeID
+    def set_EspecialidadeID(self,EspecialidadeID):
+        self.EspecialidadeID = EspecialidadeID
 
     def get_dtIni(self):
-        return self.dt_ini
-    def set_dtIni(self, dt_Ini):
-        self.dt_ini = dt_Ini
+        return self.DataInclusao
+    def set_dtIni(self, DataInclusao):
+        self.DataInclusao = DataInclusao
 
-    def get_dt_exc(self):
-        return self.dt_exc  
-    def set_dt_exc(self, dt_exc):
-        self.dt_exc = dt_exc  
+    def get_DataExclusao(self):
+        return self.DataExclusao  
+    def set_DataExclusao(self, DataExclusao):
+        self.DataExclusao = DataExclusao   
 
 

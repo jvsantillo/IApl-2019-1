8a9005c3b68aa17cb3632138b2006a7b22276f59
caioromao <cromao21@gmail.com>
2019-04-29 14:50:07 -0300

    Corrigindo nomes de atributos classe "Pessoa"
    Corrigindo nomes de atributos classe "Pessoa"

--- IApl-2019-1-SiglaProjeto/AS01/2.Sistema/pessoa.py
+++ IApl-2019-1-SiglaProjeto/AS01/2.Sistema/pessoa.py
@@ -1,31 +1,30 @@
 class Pessoa:
     
     #Construtor:
-    def __init__ (self, codigo, nome, dt_ini, dt_exec):
-        self.codigo = codigo
-        self.nome = nome
-        self.dt_ini = dt_ini
-        self.dt_exec = dt_exec    
+    def __init__ (self, PessoaID, Nome, DataInclusao, DataExclusao):
+        self.PessoaID = PessoaID
+        self.Nome = Nome
+        self.DataInclusao = DataInclusao
+        self.DataExclusao = DataExclusao    
     
     #Métodos Get e Set:
 
-    def get_name(self):
-        return self.nome
-    def set_name(self,name):
-        self.nome = name
+    def get_Nome(self):
+        return self.Nome
+    def set_Nome(self,Nome):
+        self.Nome = Nome
 
-    def get_cod(self):
-        return self.codigo
-    def set_cod(self,cod):
-        self.codigo = cod
+    def get_PessoaID(self):
+        return self.PessoaID
+    def set_PessoaID(self,PessoaID):
+        self.PessoaID = PessoaID
 
-    def get_dtIni(self):
-        return self.dt_ini
-    def set_dtIni(self, dtIni):
-        self.dt_ini = dtIni
+    def get_DataInclusao(self):
+        return self.DataInclusao
+    def set_DataInclusao(self, DataInclusao):
+        self.DataInclusao = DataInclusao
 
-    def get_dt_exec(self):
-        return self.dt_exec  
-    def set_dt_exec(self, dt_exec):
-        self.dt_exec = dt_exec
-    
+    def get_DataExclusao(self):
+        return self.DataExclusao  
+    def set_DataExclusao(self, DataExclusao):
+        self.DataExclusao = DataExclusao

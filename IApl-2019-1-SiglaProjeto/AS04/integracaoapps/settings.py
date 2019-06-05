c886a259e75dd5de7473893cfc1c791351ee72db
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2019-06-05 13:35:20 -0300

    Conexão com o banco postgreSQL

--- IApl-2019-1-SiglaProjeto/AS04/integracaoapps/settings.py
+++ IApl-2019-1-SiglaProjeto/AS04/integracaoapps/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'integracaoapps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'Django'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

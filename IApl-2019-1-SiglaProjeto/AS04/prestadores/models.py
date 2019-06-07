3c2dc8f83ccd0f4ac84b9a92b10681a4b8ca6174
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2019-06-07 14:04:29 -0300

    Finalizando models, rodando migrations

--- IApl-2019-1-SiglaProjeto/AS04/prestadores/models.py
+++ IApl-2019-1-SiglaProjeto/AS04/prestadores/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    insertion_date = models.DateTimeField('Person insertion date')
+    exclusion_date = models.DateTimeField('Person exclusion Date', null=True)
+
+#REVIEW: Is supplier and person a 1-to-1 relationship? 
+class Supplier(models.Model):
+    person = models.OneToOneField(
+        Person,
+        on_delete=models.CASCADE, 
+        primary_key=True,
+    )
+    insertion_date = models.DateTimeField('Supplier insertion date')
+    exclusion_date = models.DateTimeField('Supplier exclusion date', null=True)
+
+class Expertise(models.Model):
+    field = models.CharField(max_length=200)
+    description = models.TextField(max_length=4500)
+    insertion_date = models.DateTimeField('Expertise insertion date')
+    exclusion_date = models.DateTimeField('Expertise exclusion date', null=True) 
+    suppliers = models.ManyToManyField(Supplier) 

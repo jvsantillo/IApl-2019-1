55a9703faa133d93644c155965a37ecfcc060655
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2019-06-10 16:06:44 -0300

    Criando views para os models

--- IApl-2019-1-SiglaProjeto/AS04/prestadores/views.py
+++ IApl-2019-1-SiglaProjeto/AS04/prestadores/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. Você está na view index")
+    return HttpResponse("Hello, world. Index view")
 
-def criar(request):
-    return HttpResponse("Tela de criação de prestadores")
+def create_person(request):
+    return HttpResponse("In development... You are creating a person")
+def update_person(request, person_id):
+    return HttpResponse("In development... You are updating person ID %s." % person_id)
+def delete_person(request, person_id):
+    return HttpResponse("In development... You are deleting person ID %s." % person_id)
+def retrive_persons(request):
+    return HttpResponse("In development...Here are all persons")
+
+def create_supplier(request):
+    return HttpResponse("In development... You are creating a supplier")
+def update_supplier(request, supplier_id):
+    return HttpResponse("In development... You are updating supplier ID %s." % supplier_id)
+def delete_supplier(request, supplier_id):
+    return HttpResponse("In development... You are updating supplier ID %s." % supplier_id)
+    return HttpResponse("In development... You are deleting supplier ID %s." % supplier_id)
+def retrive_suppliers(request):
+    return HttpResponse("In development...Here are all suppliers")
+
+def create_expertise(request):
+    return HttpResponse("In development... You are creating a expertise")
+def update_expertise(request, expertise_id):
+    return HttpResponse("In development... You are updating expertise ID %s." % expertise_id)
+def delete_expertise(request, expertise_id):
+    return HttpResponse("In development... You are updating expertise ID %s." % expertise_id)
+    return HttpResponse("In development... You are deleting expertise ID %s." % expertise_id)
+def retrive_expertises(request):
+    return HttpResponse("In development...Here are all expertises")
